b8ac084753c02112b0798d418ae0064db105f39e
LuCtld <lcataldo.lc@gmail.com>
2022-07-16 18:08:54 -0500

    arrays

--- js/main.js
+++ js/main.js
@@ -1,30 +1,61 @@
-//Determina Cantidad
 
-let cantidadparafinas = prompt ("Pack parafinas $600, cuántas desea comprar?")
-let valorunpack = 600;
+//Objetos
+
+function Producto (parafina) {
+
+    this.id = parafina.id;
+
+    this.nombre = parafina.nombre;
+    
+    this.marca = parafina.marca;
+    
+    this.precio = parafina.precio;
+    
+}
+    
+const parafinaBase = new Producto ({id: 1, nombre: "base", marca: "keep surfing", precio: 400});
+    
+const parafinaSummer = new Producto ({id: 2, nombre: "summer", marca: "free life", precio: 350});
+    
+const parafinaWinter = new Producto ({id: 3, nombre: "winter", marca: "free life", precio: 500});
+    
+const parafinaUltra = new Producto ({id: 4, nombre: "ultra", marca: "keep surfing", precio: 450});
+    
+
+//Arrays
+    
+const listadoParafinas = [ parafinaBase, parafinaSummer, parafinaWinter, parafinaUltra];
+    
 
 
-function preciocompra (cantidadparafinas, valorunpack ){
-    let preciofinal = cantidadparafinas * valorunpack ;
-    console.log = preciofinal;
-    alert (" El costo de" + " " + cantidadparafinas +" "+ "pack/s es $"  + preciofinal)
+// Funcion
 
-    let comoabona = prompt ("Efectivo con descuento del 20% o Tarjeta de credito con recargo del 10%?");
+let quecompra = prompt ("Hola! que parafina querés comprar? Base, Summer, Winter, Ultra ");
+let quecantidad = prompt ("Cuántas querés comprar?");
 
-    if (comoabona == "efectivo") {
-        let preciocondescuento = preciofinal - preciofinal *(20/100);
-        console.log = preciocondescuento;
-        alert ("El precio final de su compra es $" + preciocondescuento + " " + "Gracias! vuelva pronto :)");
-        }
-        else {
-            let preciotarjeta = preciofinal + preciofinal *(20/100);
-            console.log = preciotarjeta;
-            alert ("Su compra se realizará en un pago de $" + " " + preciotarjeta + " " + "Gracias! vuelva pronto :)")
-        }
+function compra (quecompra, quecantidad){    
+    for (let i = 0; i < 5; i++){  
+        if (quecompra.toLowerCase() == listadoParafinas[i].nombre){
+            let preciofinal = quecantidad * listadoParafinas[i].precio;
+            console.log  (preciofinal);   
+            alert ("El costo de" + " " + quecantidad +" "+ "pack/s es $" + preciofinal);
+
+            let concretaCompra = prompt ("Agregar al carrito?");
+            const comprafinal = [];
+
+            if (concretaCompra.toLowerCase() === "si"){
+            
+                function agregaralcarrito (listadoParafinas){
+                    comprafinal.push (listadoParafinas[i].nombre);
+                    console.log  (comprafinal);   
+                    alert ("Se agregó" + " " + listadoParafinas[i].nombre + " " + "al carrito")
+
+                }
+            }
+
+
+        }   
     }
-
-
-preciocompra (cantidadparafinas, valorunpack);
-
-
-
+}
+compra (quecompra, quecantidad);
+agregaralcarrito (listadoParafinas);
